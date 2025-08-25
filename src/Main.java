ffe3de77d760261cd8470bbfd9e607e0609ba549
Pedro Juan <pedrosaraivaofc@gmail.com>
2025-08-25 20:35:13 -0300

    Aula 01 - Padrões de desenvolvimento e conceitos

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Olá, digite o seu nome: ");
+        String name = scanner.next();
+
+        System.out.print("Digite sua idade: ");
+        int age = scanner.nextInt();
+
+        System.out.printf("Olá %s, sua idade é %s", name, age );
+    }
+}
