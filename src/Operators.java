3a4ea18c59083921d2fa0fd60996b2b61cb22720
Pedro Juan <pedrosaraivaofc@gmail.com>
2025-08-25 21:07:29 -0300

    Aula 03 - Operadores

---
+++ src/Operators.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Operators {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean canDrive;
+        boolean isEmancipated;
+
+        System.out.print("Quantos anos você tem? ");
+        var age = scanner.nextInt();
+
+        System.out.print("Você é emancipado? ");
+        isEmancipated = scanner.nextBoolean();
+
+        canDrive = age >= 18 || isEmancipated && age >= 16;
+
+        System.out.println("Pode dirigir: " + canDrive);
+
+    }
+}
