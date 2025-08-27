6f82b6b72ff533455c4a3711aedd4aa8367544af
Pedro Juan <pedrosaraivaofc@gmail.com>
2025-08-27 16:22:39 -0300

    Aula 3 - Módulo 2: Repetição com For

---
+++ src/Modulo_02/ForRepetition.java
@@ -0,0 +1,16 @@
+public class ForRepetition {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+
+//        Sintaxe geral:
+//        for (inicialização; condição; incremento/decremento) {
+//              // bloco de código a ser repetido
+//        }
+//        - Inicialização: ocorre apenas uma vez no início do loop (ex: int i = 1)
+//        - Condição: enquanto for verdadeira, o bloco continua executando (ex: i <= 100)
+//        - Incremento/Decremento: executado a cada iteração, após o bloco de código (ex: i++)
+    }
+}
