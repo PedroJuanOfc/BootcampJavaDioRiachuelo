e174433a6182728b3e0cb1462bc3ab27bfdf64cc
Pedro Juan <pedrosaraivaofc@gmail.com>
2025-08-27 14:38:30 -0300

    Aula 04 - Operadores aritméticos

---
+++ src/Operators2.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Operators2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe o primeiro número:");
+        var value1 = scanner.nextInt();
+        System.out.println("Informe o segundo número:");
+        var value2 = scanner.nextInt();
+
+        System.out.printf("%s + %s = %s\n", value1, value2, value1 + value2);
+
+//        System.out.println("Informe o primeiro número:");
+//        var value1 = scanner.nextInt();
+//        System.out.println("Informe o segundo número:");
+//        var value2 = scanner.nextInt();
+//
+//        System.out.printf("%s - %s = %s\n", value1, value2, value1 - value2);
+
+
+//        System.out.println("Informe o primeiro número:");
+//        var value1 = scanner.nextInt();
+//        System.out.println("Informe o segundo número:");
+//        var value2 = scanner.nextInt();
+//
+//        System.out.printf("%s / %s = %s\n", value1, value2, value1 / value2);
+
+
+//        System.out.println("Informe o primeiro número:");
+//        var value1 = scanner.nextInt();
+//        System.out.println("Informe o segundo número:");
+//        var value2 = scanner.nextInt();
+//
+//        System.out.printf("%s * %s = %s\n", value1, value2, value1 * value2);
+
+
+//        System.out.println("Informe o primeiro número:");
+//        var value1 = scanner.nextInt();
+//        System.out.println("Informe o segundo número:");
+//        var value2 = scanner.nextInt();
+//
+//        System.out.printf("%s %% %s = %s\n", value1, value2, value1 % value2);
+
+    }
+}
