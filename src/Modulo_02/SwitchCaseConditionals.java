7f8ff4a2b74505a1b68b7a814583a5f9128bbfcf
Pedro Juan <pedrosaraivaofc@gmail.com>
2025-08-27 15:16:31 -0300

    Aula 2 - Módulo 2: Estrutura condicional Switch Case

---
+++ src/Modulo_02/SwitchCaseConditionals.java
@@ -0,0 +1,39 @@
+package Modulo_02;
+
+import java.util.Scanner;
+
+public class SwitchCaseConditionals {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Escolha um número de 1 a 7: ");
+        var option = sc.nextInt();
+
+        switch (option) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("Opção inválida!");
+                break;
+        }
+    }
+}
